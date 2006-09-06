0813f652a80f4c52ae2b753c98c2c399d65c3240
Brian Harring <ferringb@gentoo.org>
2006-09-06 04:26:09 -0700

    unused use.desc (global use flags) check

--- pkgcore_checks/use_flags.py
+++ pkgcore_checks/use_flags.py
@@ -1,28 +1,28 @@
 # Copyright: 2006 Brian Harring <ferringb@gmail.com>
 # License: GPL2
 
-from pkgcore_checks.base import template, profile_options, package_feed, Result
-from pkgcore_checks.util import get_use_local_desc
+from pkgcore_checks import base, util
 from pkgcore.util.lists import iflatten_instance
 from pkgcore.util.demandload import demandload
 demandload(globals(), "pkgcore.util.xml:escape ")
 
-class UnusedLocalFlagCheck(template):
+class UnusedLocalFlagsCheck(base.template):
 
     """
     check for unused use.local.desc entries
     """
 
-    feed_type = package_feed
-    requires = profile_options
+    feed_type = base.package_feed
+    requires = base.profile_options
 
     def __init__(self, options):
-        template.__init__(self, options)
+        base.template.__init__(self, options)
         self.flags = {}
 
     # we're a profile based option, thus we get extra crap we don't need
     def start(self, repo, *a):
-        self.flags = get_use_local_desc(self.options.profile_base_dir)
+        base.template.start(self, repo, *a)
+        self.flags = util.get_use_local_desc(self.options.profile_base_dir)
     
     def feed(self, pkgs, reporter):
         for restrict, flags in self.flags.get(pkgs[0].key, {}).iteritems():
@@ -32,7 +32,7 @@ class UnusedLocalFlagCheck(template):
                 reporter.add_report(UnusedLocalFlags(restrict, unused))
 
 
-class UnusedLocalFlags(Result):
+class UnusedLocalFlags(base.Result):
     
     """
     unused use.local.desc flag(s)
@@ -41,7 +41,7 @@ class UnusedLocalFlags(Result):
     __slots__ = ("category", "package", "atom", "flags")
 
     def __init__(self, restrict, flags):
-        Result.__init__(self)
+        base.Result.__init__(self)
         # tricky, but it works; atoms have the same attrs
         self._store_cp(restrict)
         self.atom = str(restrict)
@@ -65,3 +65,54 @@ class UnusedLocalFlags(Result):
 </check>""" % (self.__class__.__name__, self.category, self.package,
     escape("atom %s unused use.local.desc flags: %s" % 
 	(self.atom, ', '.join(self.flags))))
+
+
+class UnusedGlobalFlagsCheck(base.template):
+    """
+    check for unused use.desc entries
+    """
+    
+    feed_type = base.versioned_feed
+    enabling_threshold = base.repository_feed
+    requires = base.profile_options
+
+    def __init__(self, options):
+        base.template.__init__(self, options)
+        self.flags = None
+    
+    def start(self, repo, *a):
+        base.template.start(self, repo, *a)
+        self.flags = set(util.get_use_desc(self.options.profile_base_dir))
+
+    def feed(self, pkg, reporter):
+        self.flags.difference_update(pkg.iuse)
+    
+    def finish(self, reporter):
+        if self.flags:
+            reporter.add_report(UnusedGlobalFlags(self.flags))
+        self.flags.clear()
+
+
+class UnusedGlobalFlags(base.Result):
+    
+    """
+    unused use.local.desc flag(s)
+    """
+    
+    __slots__ = ("flags",)
+
+    def __init__(self, flags):
+        base.Result.__init__(self)
+        # tricky, but it works; atoms have the same attrs
+        self.flags = tuple(sorted(flags))
+    
+    def to_str(self):
+        return "use.desc unused flag(s): %s" % \
+    		', '.join(self.flags)
+
+    def to_xml(self):
+        return \
+"""<check name="%s">
+    <msg>%s</msg>
+</check>""" % (self.__class__.__name__, 
+    escape("use.desc unused flags: %s" % ', '.join(self.flags)))
