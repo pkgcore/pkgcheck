970419c9300975d292250ee3aec9d1aea866d6d3
Marien Zwart <marienz@gentoo.org>
2006-11-14 22:21:46 +0100

    Add a no-arch-checks default checkset to config.

---
+++ pkgcore/plugins/pcheck_config.py
@@ -0,0 +1,16 @@
+
+
+"""Extra default config sections from pkgcore-checks."""
+
+
+from pkgcore.config import basics
+
+
+pkgcore_plugins = {
+    'global_config': [{
+            'no-arch-checks': basics.ConfigSectionFromStringDict({
+                    'class': 'pkgcore_checks.pcheck.Blacklist',
+                    'patterns': 'unstable_only stale_unstable imlate',
+                    }),
+            }],
+    }
