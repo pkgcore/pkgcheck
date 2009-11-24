bb217553d07e8bc800ccc73e57537467041e98d3
Brian Harring <ferringb@gmail.com>
2009-11-24 03:34:36 -0800

    pull in some fixes to distutils namespace...

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ class test(snk_distutils.test):
     default_test_namespace = 'pkgcore_checks'
     blacklist = frozenset(['pkgcore_checks.plugins'])
 
-class pchecks_build_py(snk_distutils.snk_build_py):
+class pchecks_build_py(snk_distutils.build_py):
 
     package_namespace = 'pkgcore_checks'
 
