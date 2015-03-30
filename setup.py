9a2e2fb7a386fab5eb0528eb667e75584b05d974
Tim Harder <radhermit@gmail.com>
2015-03-29 23:14:36 -0400

    various setup.py info updates

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ from pkgcheck import __version__
 class mysdist(snk_distutils.sdist):
 
     package_namespace = 'pkgcheck'
-    old_verinfo = False
 
 
 packages = []
@@ -41,13 +40,18 @@ class pkgcheck_build_py(snk_distutils.build_py):
     generate_verinfo = True
 
 
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
 setup(
     name="pkgcheck",
     version=__version__,
     license="BSD/GPL2",
-    author="Brian Harring",
-    author_email="ferringb@gmail.com",
+    author="Brian Harring, Tim Harder",
+    author_email="pkgcore-dev@googlegroups.com",
     description="pkgcore-based QA utility",
+    long_description=readme,
+    url='https://github.com/pkgcore/pkgcheck',
     packages=packages,
     scripts=glob.glob("bin/*"),
     cmdclass={
@@ -58,5 +62,6 @@ setup(
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python :: 2.7',
     ],
 )
