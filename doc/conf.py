619210f182d2c9ca3cd60f119dce39d600be9cf7
Tim Harder <radhermit@gmail.com>
2015-08-23 17:39:20 -0400

    doc: fix the authors and copyright info

--- doc/conf.py
+++ doc/conf.py
@@ -83,7 +83,9 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'pkgcheck'
-copyright = '2014-2015, Tim Harder'
+authors_list = ['Brian Harring', 'Tim Harder']
+authors = ', '.join(authors_list)
+copyright = '2006-2015, pkgcheck contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -236,7 +238,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   ('index', 'pkgcheck.tex', 'pkgcheck Documentation',
-   'Tim Harder', 'manual'),
+   authors, 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -265,7 +267,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('man/pkgcheck', 'pkgcheck', import_module('pkgcheck.scripts.pkgcheck').__doc__.split('\n', 1)[0], ['Tim Harder'], 1)
+    ('man/pkgcheck', 'pkgcheck', import_module('pkgcheck.scripts.pkgcheck').__doc__.split('\n', 1)[0], authors_list, 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -280,7 +282,7 @@ generate_docs()
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'pkgcheck', 'pkgcheck Documentation',
-   'Tim Harder', 'pkgcheck', 'One line description of project.',
+   authors, 'pkgcheck', 'One line description of project.',
    'Miscellaneous'),
 ]
 
@@ -300,10 +302,10 @@ texinfo_documents = [
 # -- Options for Epub output ----------------------------------------------
 
 # Bibliographic Dublin Core info.
-epub_title = 'pkgcheck'
-epub_author = 'Tim Harder'
-epub_publisher = 'Tim Harder'
-epub_copyright = '2014-2015, Tim Harder'
+epub_title = project
+epub_author = authors
+epub_publisher = authors
+epub_copyright = copyright
 
 # The basename for the epub file. It defaults to the project name.
 #epub_basename = 'pkgcheck'
