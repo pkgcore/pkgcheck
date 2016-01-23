10ddc6615906fbd95be1a142b0e43a4644d275f1
Tim Harder <radhermit@gmail.com>
2016-01-23 13:57:21 -0500

    setup: consistent quoting

--- setup.py
+++ setup.py
@@ -18,12 +18,12 @@ with io.open('README.rst', encoding='utf-8') as f:
     readme = f.read()
 
 setup(
-    name="pkgcheck",
+    name='pkgcheck',
     version=pkgdist.version(),
-    license="BSD/GPL2",
-    author="Brian Harring, Tim Harder",
-    author_email="pkgcore-dev@googlegroups.com",
-    description="pkgcore-based QA utility",
+    license='BSD/GPL2',
+    author='Brian Harring, Tim Harder',
+    author_email='pkgcore-dev@googlegroups.com',
+    description='pkgcore-based QA utility',
     long_description=readme,
     url='https://github.com/pkgcore/pkgcheck',
     packages=find_packages(),
@@ -34,12 +34,12 @@ setup(
     ],
     scripts=os.listdir('bin'),
     cmdclass={
-        "sdist": pkgdist.sdist,
-        "test": test,
-        "build_py": pkgdist.build_py,
-        "build_man": pkgdist.build_man,
+        'sdist': pkgdist.sdist,
+        'test': test,
+        'build_py': pkgdist.build_py,
+        'build_man': pkgdist.build_man,
         'build_scripts': pkgdist.build_scripts,
-        "install_man": pkgdist.install_man,
+        'install_man': pkgdist.install_man,
     },
     classifiers=[
         'License :: OSI Approved :: BSD License',
