592c6163d6f4a34c83102fa565c051e4a4444301
Tim Harder <radhermit@gmail.com>
2016-06-09 02:07:51 -0400

    plugins/pkgcheck_config: add 'repo' checkset to run repo scope checks
    To use, run something similar to the following:
    
    pkgcheck -C repo -r gentoo
    
    which will run all global, repo scope level checks on the gentoo repo.

--- pkgcheck/plugins/pkgcheck_config.py
+++ pkgcheck/plugins/pkgcheck_config.py
@@ -5,8 +5,14 @@
 
 from pkgcore.config import basics
 
+from pkgcheck import base
+
 pkgcore_plugins = {
     'global_config': [{
+        'repo': basics.ConfigSectionFromStringDict({
+            'class': 'pkgcheck.base.Scope',
+            'scopes': str(base.repository_scope),
+            }),
         'no-arch': basics.ConfigSectionFromStringDict({
             'class': 'pkgcheck.base.Blacklist',
             'patterns': 'unstable_only stale_unstable imlate',
