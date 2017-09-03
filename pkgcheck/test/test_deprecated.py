28043a63f8c7f13b9e2414a29c1e330d52de23da
Tim Harder <radhermit@gmail.com>
2017-09-03 11:07:24 -0400

    test/test_deprecated: add some more deprecated eclass check tests

--- pkgcheck/test/test_deprecated.py
+++ pkgcheck/test/test_deprecated.py
@@ -1,10 +1,12 @@
+import random
+
+from pkgcheck import deprecated
 from pkgcheck.test import misc
-from pkgcheck.deprecated import DeprecatedEclassReport as dep_eclass
 
 
 class TestDeprecatedEclass(misc.ReportTestCase):
 
-    check_kls = dep_eclass
+    check_kls = deprecated.DeprecatedEclassReport
 
     def mk_pkg(self, ver, eclasses):
         return misc.FakePkg(
@@ -12,9 +14,31 @@ class TestDeprecatedEclass(misc.ReportTestCase):
             data={"_eclasses_": {}.fromkeys(eclasses)})
 
     def test_it(self):
-        # single version, shouldn't yield.
-        check = dep_eclass(None, None)
+        check = deprecated.DeprecatedEclassReport(None, None)
+
+        # no eclasses
         self.assertNoReport(check, self.mk_pkg("0.7.1", []))
-        reports = self.assertReports(
-            check, self.mk_pkg("0.1", check.blacklist))
-        self.assertEqual(len(reports), 1)
+
+        # one non-deprecated eclass
+        self.assertNoReport(check, self.mk_pkg("0.7.1", {'foobar': None}))
+
+        # one deprecated eclass
+        eclasses = dict([check.blacklist.iteritems().next()])
+        r = self.assertReport(check, self.mk_pkg("0.1", eclasses))
+        self.assertIsInstance(r, deprecated.DeprecatedEclass)
+        self.assertEqual(r.eclasses, tuple(eclasses.iteritems()))
+
+        # mix of deprecated and non-deprecated eclasses
+        current = {str(x): None for x in range(3)}
+        old = {x: check.blacklist[x] for x in random.sample(check.blacklist, 3)}
+        eclasses = current.copy()
+        eclasses.update(old)
+        pkg = self.mk_pkg("0.1", eclasses)
+        self.assertEqual(pkg.inherited, tuple(sorted(eclasses)))
+        r = self.assertReport(check, pkg)
+        self.assertEqual(r.eclasses, tuple(sorted(old.iteritems())))
+
+        # all known, deprecated eclasses
+        r = self.assertReport(check, self.mk_pkg("0.1", check.blacklist))
+        self.assertIsInstance(r, deprecated.DeprecatedEclass)
+        self.assertEqual(r.eclasses, tuple(sorted((check.blacklist.iteritems()))))
