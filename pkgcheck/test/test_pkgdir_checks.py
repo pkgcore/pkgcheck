e3fb09bc9d69ba0e19d412a2ab5f6dc60f9d40d6
Tim Harder <radhermit@gmail.com>
2017-09-09 09:42:31 -0400

    test.test_pkgdir_checks: restructure to use base test class
    Which among other things makes sure that temp dirs/files are removed on
    teardown.

--- pkgcheck/test/test_pkgdir_checks.py
+++ pkgcheck/test/test_pkgdir_checks.py
@@ -9,8 +9,22 @@ from pkgcheck import pkgdir_checks
 from pkgcheck.test import misc
 
 
-class filesdir_mixin(TempDirMixin):
+class PkgDirReportTest(TempDirMixin, misc.ReportTestCase):
     """Various FILESDIR related test support."""
+
+    check_kls = pkgdir_checks.PkgDirReport
+
+    def setUp(self):
+        TempDirMixin.setUp(self)
+        self.check = pkgdir_checks.PkgDirReport(None, None)
+
+    def tearDown(self):
+        TempDirMixin.tearDown(self)
+
+    def mk_pkg(self, files={}):
+        return misc.FakeFilesDirPkg(
+            "dev-util/diffball-0.7.1",
+            self.get_pkgdir_with_filesdir(files))
 
     def get_pkgdir_with_filesdir(self, files={}):
         """Create a temporary directory for the ebuild with files/ subdirectory.
@@ -26,33 +40,26 @@ class filesdir_mixin(TempDirMixin):
         return ebuild_base
 
 
-class TestDuplicateFilesReport(filesdir_mixin, misc.ReportTestCase):
-
-    check_kls = pkgdir_checks.PkgDirReport
-
-    def mk_pkg(self, files={}):
-        return misc.FakeFilesDirPkg(
-            "dev-util/diffball-0.7.1",
-            self.get_pkgdir_with_filesdir(files))
+class TestDuplicateFilesReport(PkgDirReportTest):
+    """Check DuplicateFiles results."""
 
     def test_it(self):
-        check = pkgdir_checks.PkgDirReport(None, None)
-
         # empty filesdir
-        self.assertNoReport(check, [self.mk_pkg()])
+        self.assertNoReport(self.check, [self.mk_pkg()])
 
         # filesdir with two unique files
-        self.assertNoReport(check, [self.mk_pkg({'test': 'abc', 'test2': 'bcd'})])
+        self.assertNoReport(self.check, [self.mk_pkg({'test': 'abc', 'test2': 'bcd'})])
 
         # filesdir with a duplicate
         r = self.assertIsInstance(
-            self.assertReport(check, [self.mk_pkg({'test': 'abc', 'test2': 'abc'})]),
+            self.assertReport(self.check, [self.mk_pkg({'test': 'abc', 'test2': 'abc'})]),
             pkgdir_checks.DuplicateFiles)
-        r = self.assertEqual(r.files, ('files/test', 'files/test2'))
+        self.assertEqual(r.files, ('files/test', 'files/test2'))
+        self.assertEqual(r.files, ('files/test', 'files/test2'))
 
-        # two sets of duplicates
-        r = self.assertReports(check, [self.mk_pkg(
-            {'test': 'abc', 'test2': 'abc', 'test3': 'bcd', 'test4': 'bcd'})])
+        # two sets of duplicates and one unique
+        r = self.assertReports(self.check, [self.mk_pkg(
+            {'test': 'abc', 'test2': 'abc', 'test3': 'bcd', 'test4': 'bcd', 'test5': 'zzz'})])
         self.assertLen(r, 2)
         self.assertIsInstance(r[0], pkgdir_checks.DuplicateFiles)
         self.assertIsInstance(r[1], pkgdir_checks.DuplicateFiles)
@@ -62,41 +69,33 @@ class TestDuplicateFilesReport(filesdir_mixin, misc.ReportTestCase):
         )
 
 
-class TestEmptyFileReport(filesdir_mixin, misc.ReportTestCase):
-
-    check_kls = pkgdir_checks.PkgDirReport
-
-    def mk_pkg(self, files={}):
-        return misc.FakeFilesDirPkg(
-            "dev-util/diffball-0.7.1",
-            self.get_pkgdir_with_filesdir(files))
+class TestEmptyFileReport(PkgDirReportTest):
+    """Check EmptyFile results."""
 
     def test_it(self):
-        check = pkgdir_checks.PkgDirReport(None, None)
-
         # empty filesdir
-        self.assertNoReport(check, [self.mk_pkg()])
+        self.assertNoReport(self.check, [self.mk_pkg()])
 
         # filesdir with an empty file
         self.assertIsInstance(
-            self.assertReport(check, [self.mk_pkg({'test': ''})]),
+            self.assertReport(self.check, [self.mk_pkg({'test': ''})]),
             pkgdir_checks.EmptyFile)
 
         # filesdir with a non-empty file
-        self.assertNoReport(check, [self.mk_pkg({'test': 'asdfgh'})])
+        self.assertNoReport(self.check, [self.mk_pkg({'test': 'asdfgh'})])
 
         # a mix of both
         r = self.assertIsInstance(
-            self.assertReport(check, [self.mk_pkg({'test': 'asdfgh', 'test2': ''})]),
+            self.assertReport(self.check, [self.mk_pkg({'test': 'asdfgh', 'test2': ''})]),
             pkgdir_checks.EmptyFile)
         self.assertEqual(r.filename, 'files/test2')
         r = self.assertIsInstance(
-            self.assertReport(check, [self.mk_pkg({'test': '', 'test2': 'asdfgh'})]),
+            self.assertReport(self.check, [self.mk_pkg({'test': '', 'test2': 'asdfgh'})]),
             pkgdir_checks.EmptyFile)
         self.assertEqual(r.filename, 'files/test')
 
         # two empty files
-        r = self.assertReports(check, [self.mk_pkg({'test': '', 'test2': ''})])
+        r = self.assertReports(self.check, [self.mk_pkg({'test': '', 'test2': ''})])
         self.assertLen(r, 2)
         self.assertIsInstance(r[0], pkgdir_checks.EmptyFile)
         self.assertIsInstance(r[1], pkgdir_checks.EmptyFile)
