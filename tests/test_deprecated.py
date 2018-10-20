0a7c5dff4101e71bfa1182eef1493b406468c99a
Tim Harder <radhermit@gmail.com>
2018-10-20 17:01:22 -0500

    tests: split up and migrate some tests to pytest syntax

--- tests/test_deprecated.py
+++ tests/test_deprecated.py
@@ -4,53 +4,51 @@ from pkgcheck import deprecated
 from pkgcheck.test import misc
 
 
+def mk_pkg(ver, eclasses):
+    return misc.FakePkg(
+        f"dev-util/diffball-{ver}", data={"_eclasses_": {}.fromkeys(eclasses)})
+
+
 class TestDeprecatedEclass(misc.ReportTestCase):
 
     check_kls = deprecated.DeprecatedEclassReport
+    check = deprecated.DeprecatedEclassReport(None, None)
 
-    def mk_pkg(self, ver, eclasses):
-        return misc.FakePkg(
-            f"dev-util/diffball-{ver}",
-            data={"_eclasses_": {}.fromkeys(eclasses)})
+    def test_no_eclasses(self):
+        self.assertNoReport(self.check, mk_pkg("0.7.1", []))
 
-    def test_it(self):
-        check = deprecated.DeprecatedEclassReport(None, None)
+    def test_single_current_eclass(self):
+        self.assertNoReport(self.check, mk_pkg("0.7.1", {'foobar': None}))
 
-        # no eclasses
-        self.assertNoReport(check, self.mk_pkg("0.7.1", []))
-
-        # one non-deprecated eclass
-        self.assertNoReport(check, self.mk_pkg("0.7.1", {'foobar': None}))
-
-        # deprecated eclass with no replacement
+    def test_deprecated_no_replacement(self):
         eclass, replacement = next(
-            (k, v) for k, v in check.blacklist.items() if v == None)
+            (k, v) for k, v in self.check.blacklist.items() if v == None)
         eclasses = {eclass: replacement}
-        r = self.assertReport(check, self.mk_pkg("0.1", eclasses))
+        r = self.assertReport(self.check, mk_pkg("0.1", eclasses))
         assert isinstance(r, deprecated.DeprecatedEclass)
         assert r.eclasses == tuple(eclasses.items())
         assert f"uses deprecated eclass: [ {eclass} (no replacement) ]" == str(r)
 
-        # deprecated eclass with replacement
+    def test_deprecated_with_replacement(self):
         eclass, replacement = next(
-            (k, v) for k, v in check.blacklist.items() if v)
+            (k, v) for k, v in self.check.blacklist.items() if v)
         eclasses = {eclass: replacement}
-        r = self.assertReport(check, self.mk_pkg("0.1", eclasses))
+        r = self.assertReport(self.check, mk_pkg("0.1", eclasses))
         assert isinstance(r, deprecated.DeprecatedEclass)
         assert r.eclasses == tuple(eclasses.items())
         assert f"uses deprecated eclass: [ {eclass} (migrate to {replacement}) ]" == str(r)
 
-        # mix of deprecated and non-deprecated eclasses
+    def test_deprecated_and_current(self):
         current = {str(x): None for x in range(3)}
-        old = {x: check.blacklist[x] for x in random.sample(list(check.blacklist), 3)}
+        old = {x: self.check.blacklist[x] for x in random.sample(list(self.check.blacklist), 3)}
         eclasses = current.copy()
         eclasses.update(old)
-        pkg = self.mk_pkg("0.1", eclasses)
+        pkg = mk_pkg("0.1", eclasses)
         assert pkg.inherited == tuple(sorted(eclasses))
-        r = self.assertReport(check, pkg)
+        r = self.assertReport(self.check, pkg)
         assert r.eclasses == tuple(sorted(old.items()))
 
-        # all known, deprecated eclasses
-        r = self.assertReport(check, self.mk_pkg("0.1", check.blacklist))
+    def test_all_known_deprecated(self):
+        r = self.assertReport(self.check, mk_pkg("0.1", self.check.blacklist))
         assert isinstance(r, deprecated.DeprecatedEclass)
-        assert r.eclasses == tuple(sorted(check.blacklist.items()))
+        assert r.eclasses == tuple(sorted(self.check.blacklist.items()))
--- tests/test_glsa.py
+++ tests/test_glsa.py
@@ -1,31 +1,39 @@
 from pkgcore.test.misc import mk_glsa
+import pytest
 from snakeoil.osutils import pjoin
-from snakeoil.test.mixins import TempDirMixin
 
 from pkgcheck.glsa import TreeVulnerabilitiesReport as vuln_report
 from pkgcheck.test import misc
 
 
-class TestVulnerabilitiesReport(TempDirMixin, misc.ReportTestCase):
+def mk_pkg(ver, key="dev-util/diffball"):
+    return misc.FakePkg(f"{key}-{ver}")
+
+
+@pytest.fixture
+def check(tmpdir):
+    check = vuln_report(
+        misc.Options(glsa_location=str(tmpdir), glsa_enabled=True))
+
+    with open(pjoin(str(tmpdir), "glsa-200611-01.xml"), "w") as f:
+        f.write(mk_glsa(("dev-util/diffball", ([], [">0.7"]))))
+    with open(pjoin(str(tmpdir), "glsa-200611-02.xml"), "w") as f:
+        f.write(mk_glsa(("dev-util/diffball", ([], ["~>=0.5-r3"]))))
+    return check
+
+
+class TestVulnerabilitiesReport(misc.ReportTestCase):
 
     check_kls = vuln_report
 
-    def mk_pkg(self, ver, key="dev-util/diffball"):
-        return misc.FakePkg(f"{key}-{ver}")
+    def test_non_matching(self, check):
+        self.assertNoReport(check, mk_pkg("0.5.1"))
 
-    def test_it(self):
-        # single version, shouldn't yield.
-        check = vuln_report(
-            misc.Options(glsa_location=self.dir, glsa_enabled=True))
-        with open(pjoin(self.dir, "glsa-200611-01.xml"), "w") as f:
-            f.write(mk_glsa(("dev-util/diffball", ([], [">0.7"]))))
-        with open(pjoin(self.dir, "glsa-200611-02.xml"), "w") as f:
-            f.write(mk_glsa(("dev-util/diffball", ([], ["~>=0.5-r3"]))))
-        self.assertNoReport(check, self.mk_pkg("0.5.1"))
-        r = self.assertReports(check, self.mk_pkg("0.5-r5"))
+    def test_matching(self, check):
+        r = self.assertReports(check, mk_pkg("0.5-r5"))
         assert len(r) == 1
         assert (
             (r[0].category, r[0].package, r[0].version) ==
             ("dev-util", "diffball", "0.5-r5"))
-        self.assertReports(check, self.mk_pkg("1.0"))
-        self.assertNoReport(check, self.mk_pkg("5", "dev-util/diffball2"))
+        self.assertReports(check, mk_pkg("1.0"))
+        self.assertNoReport(check, mk_pkg("5", "dev-util/diffball2"))
