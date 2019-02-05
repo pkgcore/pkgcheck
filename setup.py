049828c327a450ab8087c537be16e7dd571c5da3
Tim Harder <radhermit@gmail.com>
2019-02-04 18:05:19 -0600

    setup.py: generate plugin module cache

--- setup.py
+++ setup.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python3
 
+from distutils import log
+import os
+
 from setuptools import setup
 
 from snakeoil.dist import distutils_extensions as pkgdist
 pkgdist_setup, pkgdist_cmds = pkgdist.setup()
+
+
+class install(pkgdist.install):
+    """Install wrapper to generate and install pkgcheck-related files."""
+
+    def run(self):
+        pkgdist.install.run(self)
+        if not self.dry_run:
+            # Install module plugincache
+            # TODO: move this to pkgdist once plugin support is moved to snakeoil
+            with pkgdist.syspath(pkgdist.PACKAGEDIR):
+                from pkgcheck import plugins
+                from pkgcore import plugin
+                log.info('Generating plugin cache')
+                path = os.path.join(self.install_purelib, 'pkgcheck', 'plugins')
+                plugin.initialize_cache(plugins, force=True, cache_dir=path)
 
 
 setup(**dict(pkgdist_setup,
@@ -17,10 +36,12 @@ setup(**dict(pkgdist_setup,
     cmdclass=dict(
         pkgdist_cmds,
         test=pkgdist.pytest,
+        install=install,
         ),
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         ],
     )
 )
