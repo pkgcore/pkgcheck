779f28983f6c932d5a6c638fcc0c6a452f8b1602
Georgy Yakovlev <gyakovlev@gentoo.org>
2019-05-08 17:28:41 -0700

    checks.metadata_checks: add preserve-libs to known restricts

--- src/pkgcheck/checks/metadata_checks.py
+++ src/pkgcheck/checks/metadata_checks.py
@@ -848,7 +848,7 @@ class RestrictsReport(base.Template):
     feed_type = base.versioned_feed
     known_restricts = frozenset((
         "binchecks", "bindist", "fetch", "installsources", "mirror",
-        "primaryuri", "splitdebug", "strip", "test", "userpriv",
+        "preserve-libs", "primaryuri", "splitdebug", "strip", "test", "userpriv",
     ))
 
     known_results = (BadRestricts,) + addons.UseAddon.known_results
