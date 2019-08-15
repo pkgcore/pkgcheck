c7e85829b74c3b7b1744c92bd267645418eb435f
Tim Harder <radhermit@gmail.com>
2019-08-15 03:38:14 -0600

    tests: add initial checks for PackageUpdatesCheck

---
+++ tests/module/checks/test_repo_metadata.py
@@ -0,0 +1,85 @@
+import os
+import uuid
+
+from pkgcore.ebuild import repository, repo_objs
+from pkgcore.test.misc import FakePkg
+from snakeoil.fileutils import touch
+from snakeoil.osutils import pjoin
+
+from pkgcheck.checks import repo_metadata
+
+from .. import misc
+
+
+class TestPackageUpdatesCheck(misc.Tmpdir, misc.ReportTestCase):
+
+    check_kls = repo_metadata.PackageUpdatesCheck
+
+    def mk_check(self, pkgs=(), **kwargs):
+        # TODO: switch to using a repo fixture when available
+        repo_dir = pjoin(self.dir, uuid.uuid4().hex)
+        os.makedirs(pjoin(repo_dir, 'metadata'))
+        with open(pjoin(repo_dir, 'metadata', 'layout.conf'), 'w') as f:
+            f.write('masters =\n')
+
+        os.makedirs(pjoin(repo_dir, 'profiles', 'updates'))
+        with open(pjoin(repo_dir, 'profiles', 'repo_name'), 'w') as f:
+            f.write('fake\n')
+        for filename, updates in kwargs.items():
+            with open(pjoin(repo_dir, 'profiles', 'updates', filename), 'w') as f:
+                f.write('\n'.join(updates))
+
+        for pkg in pkgs:
+            pkg = FakePkg(pkg)
+            pkg_path = pjoin(
+                repo_dir, pkg.category, pkg.package, f'{pkg.package}-{pkg.fullver}.ebuild')
+            os.makedirs(os.path.dirname(pkg_path), exist_ok=True)
+            touch(pkg_path)
+
+        repo_config = repo_objs.RepoConfig(location=repo_dir)
+        repo = repository.UnconfiguredTree(repo_config.location, repo_config=repo_config)
+        options = misc.Options(target_repo=repo)
+        return self.check_kls(options)
+
+    def test_no_updates(self):
+        # no update files
+        self.assertNoReport(self.mk_check(), [])
+
+        # empty file
+        updates = {'1Q-2020': []}
+        self.assertNoReport(self.mk_check(**updates), [])
+
+    def test_bad_update_filenames(self):
+        # only files named using the format [1-4]Q-[YYYY] are allowed
+        updates = {'foobar': ['blah']}
+        r = self.assertReport(self.mk_check(**updates), [])
+        assert isinstance(r, repo_metadata.BadPackageUpdate)
+        assert "incorrectly named update file: 'foobar'" == str(r)
+
+        updates = {'5Q-2020': ['blah']}
+        r = self.assertReport(self.mk_check(**updates), [])
+        assert isinstance(r, repo_metadata.BadPackageUpdate)
+        assert "incorrectly named update file: '5Q-2020'" == str(r)
+
+        # hidden files will be flagged
+        updates = {'.1Q-2020.swp': ['blah']}
+        r = self.assertReport(self.mk_check(**updates), [])
+        assert isinstance(r, repo_metadata.BadPackageUpdate)
+        assert "incorrectly named update file: '.1Q-2020.swp'" == str(r)
+
+    def test_empty_line(self):
+        updates = {'1Q-2020': ['  ']}
+        r = self.assertReport(self.mk_check(**updates), [])
+        assert isinstance(r, repo_metadata.BadPackageUpdate)
+        assert "file '1Q-2020': empty line 1" == str(r)
+
+    def test_extra_whitespae(self):
+        # whitespace prefix
+        for update in (' move dev-util/foo dev-util/bar',  # prefix
+                      'move dev-util/foo dev-util/bar '):  # suffix
+            updates = {'1Q-2020': [update]}
+            pkgs = ('dev-util/foo-0', 'dev-util/bar-1')
+            r = self.assertReport(self.mk_check(pkgs=pkgs, **updates), [])
+            assert isinstance(r, repo_metadata.BadPackageUpdate)
+            assert 'extra whitespace' in str(r)
+            assert 'on line 1' in str(r)
