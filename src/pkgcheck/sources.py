28e75e860f32ebfb964c310cd108034850c9bc86
Tim Harder <radhermit@gmail.com>
2019-08-28 20:49:35 -0600

    sources: RawRepoSource: use custom repo for pkg generation
    That allows matching against mismatched and invalid package names. The
    regular class from pkgcore ignores pkgs with those issues entirely when
    generating available pkgs.
    
    This fixes flagging mismatched package names where there is only one
    ebuild in a package dir with a non-matching name. Previously those cases
    were not caught.

--- src/pkgcheck/sources.py
+++ src/pkgcheck/sources.py
@@ -1,14 +1,46 @@
 """Custom package sources used for feeding addons."""
 
+from pkgcore.ebuild.repository import UnconfiguredTree
 from pkgcore.restrictions import packages
+from snakeoil.osutils import listdir_files, pjoin
 
 from . import addons, base
+
+
+class _RawRepo(UnconfiguredTree):
+    """Repository that allows allows matching against mismatched/invalid package names."""
+
+    def __init__(self, repo):
+        super().__init__(repo.location)
+
+    def _get_versions(self, catpkg):
+        """Pass through all packages that end with ".ebuild" extension.
+
+        Deviates from parent in that no package name check is done.
+        """
+        cppath = pjoin(self.base, catpkg[0], catpkg[1])
+        pkg = f'{catpkg[-1]}-'
+        lp = len(pkg)
+        extension = self.extension
+        ext_len = -len(extension)
+        try:
+            return tuple(
+                x[lp:ext_len] for x in listdir_files(cppath)
+                if x[ext_len:] == extension)
+        except EnvironmentError as e:
+            raise KeyError(
+                "failed fetching versions for package %s: %s" %
+                (pjoin(self.base, '/'.join(catpkg)), str(e))) from e
 
 
 class RawRepoSource(base.GenericSource):
     """Ebuild repository source returning raw CPV objects."""
 
     feed_type = base.raw_versioned_feed
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.repo = _RawRepo(self.repo)
 
     def __iter__(self):
         yield from self.repo.itermatch(
