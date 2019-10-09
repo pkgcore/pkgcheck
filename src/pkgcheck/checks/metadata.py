16e0b1dc4b82841ed41e42f7ce831ff364f42539
Tim Harder <radhermit@gmail.com>
2019-10-09 10:43:36 -0600

    SrcUriCheck: update bad filenames regex for pkgcore eapi exts regex update

--- src/pkgcheck/checks/metadata.py
+++ src/pkgcheck/checks/metadata.py
@@ -1016,7 +1016,7 @@ class SrcUriCheck(Check):
             PN = re.escape(pkg.PN)
             PV = re.escape(pkg.PV)
             exts = pkg.eapi.archive_suffixes_re.pattern
-            bad_filenames_re = rf'^({PN}|v?{PV}|[0-9a-f]{{40}}){exts}'
+            bad_filenames_re = rf'^({PN}|v?{PV}|[0-9a-f]{{40}}){exts}$'
             if re.match(bad_filenames_re, f_inst.filename):
                 bad_filenames.add(f_inst.filename)
 
