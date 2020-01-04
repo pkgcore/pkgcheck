de5d95293f44b3e449f2a0d7d86daf1abbec1cf8
Tim Harder <radhermit@gmail.com>
2020-01-03 17:48:58 -0700

    base: use 'repo' for repository scope description

--- src/pkgcheck/base.py
+++ src/pkgcheck/base.py
@@ -64,7 +64,7 @@ class Scope:
 version_scope = Scope('version', 0)
 package_scope = Scope('package', 1)
 category_scope = Scope('category', 2)
-repository_scope = Scope('repository', 3)
+repository_scope = Scope('repo', 3)
 commit_scope = Scope('commit', 4)
 
 # mapping for -S/--scopes option, ordered for sorted output in the case of unknown scopes
