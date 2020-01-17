b3e01c58ac99e7f6e1e13a04cb18d63b326e6887
Tim Harder <radhermit@gmail.com>
2020-01-16 23:54:38 -0700

    checks.network: refactor results to use more generic classes
    With the related attribute noted.

--- src/pkgcheck/checks/network.py
+++ src/pkgcheck/checks/network.py
@@ -5,7 +5,6 @@ from functools import partial
 from itertools import chain
 
 from pkgcore.fetch import fetchable
-from snakeoil.iterables import partition
 
 from .. import addons, base, results, sources
 from . import NetworkCheck
@@ -14,77 +13,65 @@ from . import NetworkCheck
 class _UrlResult(results.FilteredVersionResult, results.Warning):
     """Generic result for a URL with some type of failed status."""
 
-    def __init__(self, url, message, **kwargs):
+    def __init__(self, attr, url, message, **kwargs):
         super().__init__(**kwargs)
+        self.attr = attr
         self.url = url
         self.message = message
 
     @property
     def desc(self):
-        return f'{self.message}: {self.url}'
+        return f'{self.attr}: {self.message}: {self.url}'
 
 
-class DeadHomepage(_UrlResult):
-    """Package with a dead HOMEPAGE."""
-
-
-class DeadSrcUrl(_UrlResult):
-    """Package with a dead SRC_URI target."""
-
-
-class DeadMetadataUrl(_UrlResult):
-    """Package with a dead metadata.xml URL."""
+class DeadUrl(_UrlResult):
+    """Package with a dead URL of some type."""
 
 
 class _RedirectedUrlResult(results.FilteredVersionResult, results.Warning):
     """Generic result for a URL that permanently redirects to a different site."""
 
-    def __init__(self, url, new_url, **kwargs):
+    def __init__(self, attr, url, new_url, **kwargs):
         super().__init__(**kwargs)
+        self.attr = attr
         self.url = url
         self.new_url = new_url
 
     @property
     def desc(self):
-        return f'permanently redirected: {self.url} -> {self.new_url}'
+        return f'{self.attr}: permanently redirected: {self.url} -> {self.new_url}'
 
 
-class RedirectedHomepage(_RedirectedUrlResult):
-    """Package with a HOMEPAGE that permanently redirects to a different site."""
-
-
-class RedirectedSrcUrl(_RedirectedUrlResult):
-    """Package with a SRC_URI target that permanently redirects to a different site."""
-
-
-class RedirectedMetadataUrl(_RedirectedUrlResult):
-    """Package with a metadata.xml URL that permanently redirects to a different site."""
+class RedirectedUrl(_RedirectedUrlResult):
+    """Package with a URL that permanently redirects to a different site."""
 
 
 class SSLCertificateError(results.FilteredVersionResult, results.Warning):
     """Package with https:// HOMEPAGE with an invalid SSL cert."""
 
-    def __init__(self, url, message, **kwargs):
+    def __init__(self, attr, url, message, **kwargs):
         super().__init__(**kwargs)
+        self.attr = attr
         self.url = url
         self.message = message
 
     @property
     def desc(self):
-        return f'SSL cert error: {self.message}: {self.url}'
+        return f'{self.attr}: SSL cert error: {self.message}: {self.url}'
 
 
 class HttpsUrlAvailable(results.FilteredVersionResult, results.Warning):
     """URL uses http:// when https:// is available."""
 
-    def __init__(self, http_url, https_url, **kwargs):
+    def __init__(self, attr, http_url, https_url, **kwargs):
         super().__init__(**kwargs)
+        self.attr = attr
         self.http_url = http_url
         self.https_url = https_url
 
     @property
     def desc(self):
-        return f'HTTPS url available: {self.http_url} -> {self.https_url}'
+        return f'{self.attr}: HTTPS url available: {self.http_url} -> {self.https_url}'
 
 
 class _RequestException(Exception):
@@ -111,12 +98,11 @@ class RequestError(_RequestException):
 class _UrlCheck(NetworkCheck):
     """Various URL related checks that require internet access."""
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.dead_result = None
-        self.redirected_result = None
+    known_results = frozenset([
+        DeadUrl, RedirectedUrl, HttpsUrlAvailable, SSLCertificateError,
+    ])
 
-    def _http_check(self, url, *, pkg):
+    def _http_check(self, attr, url, *, pkg):
         """Check http:// and https:// URLs using requests."""
         result = None
         try:
@@ -131,19 +117,19 @@ class _UrlCheck(NetworkCheck):
 
             if redirected_url:
                 if redirected_url.startswith('https://') and url.startswith('http://'):
-                    result = HttpsUrlAvailable(url, redirected_url, pkg=pkg)
+                    result = HttpsUrlAvailable(attr, url, redirected_url, pkg=pkg)
                 elif redirected_url.startswith('http://') and hsts:
                     redirected_url = f'https://{redirected_url[7:]}'
-                    result = self.redirected_result(url, redirected_url, pkg=pkg)
+                    result = RedirectedUrl(attr, url, redirected_url, pkg=pkg)
                 else:
-                    result = self.redirected_result(url, redirected_url, pkg=pkg)
+                    result = RedirectedUrl(attr, url, redirected_url, pkg=pkg)
         except SSLError as e:
-            result = SSLCertificateError(url, str(e), pkg=pkg)
+            result = SSLCertificateError(attr, url, str(e), pkg=pkg)
         except RequestError as e:
-            result = self.dead_result(url, str(e), pkg=pkg)
+            result = DeadUrl(attr, url, str(e), pkg=pkg)
         return result
 
-    def _https_available_check(self, url, *, future, orig_url, pkg):
+    def _https_available_check(self, attr, url, *, future, orig_url, pkg):
         """Check if https:// alternatives exist for http:// URLs."""
         result = None
         try:
@@ -160,25 +146,25 @@ class _UrlCheck(NetworkCheck):
             if not isinstance(future.result(), HttpsUrlAvailable):
                 if redirected_url:
                     if redirected_url.startswith('https://'):
-                        result = HttpsUrlAvailable(orig_url, redirected_url, pkg=pkg)
+                        result = HttpsUrlAvailable(attr, orig_url, redirected_url, pkg=pkg)
                     elif redirected_url.startswith('http://') and hsts:
                         redirected_url = f'https://{redirected_url[7:]}'
-                        result = HttpsUrlAvailable(orig_url, redirected_url, pkg=pkg)
+                        result = HttpsUrlAvailable(attr, orig_url, redirected_url, pkg=pkg)
                 else:
-                    result = HttpsUrlAvailable(orig_url, url, pkg=pkg)
+                    result = HttpsUrlAvailable(attr, orig_url, url, pkg=pkg)
         except (RequestError, SSLError) as e:
             pass
         return result
 
-    def _ftp_check(self, url, *, pkg):
+    def _ftp_check(self, attr, url, *, pkg):
         """Check ftp:// URLs using urllib."""
         result = None
         try:
             response = urllib.request.urlopen(url, timeout=self.timeout)
         except urllib.error.URLError as e:
-            result = self.dead_result(url, str(e.reason), pkg=pkg)
+            result = DeadUrl(attr, url, str(e.reason), pkg=pkg)
         except socket.timeout as e:
-            result = self.dead_result(url, str(e), pkg=pkg)
+            result = DeadUrl(attr, url, str(e), pkg=pkg)
         return result
 
     def task_done(self, results_q, pkg, future):
@@ -194,61 +180,53 @@ class _UrlCheck(NetworkCheck):
     def _get_urls(self, pkg):
         raise NotImplementedError
 
-    def _schedule_check(self, func, url, executor, futures, results_q, **kwargs):
+    def _schedule_check(self, func, attr, url, executor, futures, results_q, **kwargs):
         future = futures.get(url)
         if future is None:
-            future = executor.submit(func, url, **kwargs)
+            future = executor.submit(func, attr, url, **kwargs)
             future.add_done_callback(partial(self.task_done, results_q, None))
             futures[url] = future
         else:
             future.add_done_callback(partial(self.task_done, results_q, kwargs['pkg']))
 
     def schedule(self, pkg, executor, futures, results_q):
-        http_urls, ftp_urls = partition(
-            self._get_urls(pkg), predicate=lambda x: x.startswith('ftp://'))
+        http_urls = []
+        for attr, url in self._get_urls(pkg):
+            if url.startswith('ftp://'):
+                self._schedule_check(
+                    self._ftp_check, attr, url, executor, futures, results_q, pkg=pkg)
+            else:
+                self._schedule_check(
+                    self._http_check, attr, url, executor, futures, results_q, pkg=pkg)
+                http_urls.append((attr, url))
+
         http_urls = tuple(http_urls)
-
-        for urls, func in ((http_urls, self._http_check),
-                           (ftp_urls, self._ftp_check)):
-            for url in urls:
-                self._schedule_check(func, url, executor, futures, results_q, pkg=pkg)
-
         http_to_https_urls = (
-            (url, f'https://{url[7:]}') for url in http_urls if url.startswith('http://'))
-        for orig_url, url in http_to_https_urls:
+            (attr, url, f'https://{url[7:]}') for (attr, url) in http_urls
+            if url.startswith('http://'))
+        for attr, orig_url, url in http_to_https_urls:
             future = futures[orig_url]
             self._schedule_check(
-                self._https_available_check, url, executor, futures, results_q,
+                self._https_available_check, attr, url, executor, futures, results_q,
                 future=future, orig_url=orig_url, pkg=pkg)
 
 
 class HomepageUrlCheck(_UrlCheck):
     """Various HOMEPAGE related checks that require internet access."""
 
-    known_results = frozenset([
-        DeadHomepage, RedirectedHomepage, HttpsUrlAvailable, SSLCertificateError])
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.dead_result = DeadHomepage
-        self.redirected_result = RedirectedHomepage
-
     def _get_urls(self, pkg):
-        yield from pkg.homepage
+        for url in pkg.homepage:
+            yield 'HOMEPAGE', url
 
 
 class FetchablesUrlCheck(_UrlCheck):
     """Various SRC_URI related checks that require internet access."""
 
-    known_results = frozenset([
-        DeadSrcUrl, RedirectedSrcUrl, HttpsUrlAvailable, SSLCertificateError])
     required_addons = (addons.UseAddon,)
 
     def __init__(self, *args, use_addon, **kwargs):
         super().__init__(*args, **kwargs)
         self.fetch_filter = use_addon.get_filter('fetchables')
-        self.dead_result = DeadSrcUrl
-        self.redirected_result = RedirectedSrcUrl
 
     def _get_urls(self, pkg):
         # ignore conditionals
@@ -258,7 +236,8 @@ class FetchablesUrlCheck(_UrlCheck):
                 pkg, allow_missing_checksums=True,
                 ignore_unknown_mirrors=True, skip_default_mirrors=True))
         for f in fetchables.keys():
-            yield from f.uri
+            for url in f.uri:
+                yield 'SRC_URI', url
 
 
 class MetadataUrlCheck(_UrlCheck):
@@ -266,14 +245,6 @@ class MetadataUrlCheck(_UrlCheck):
 
     scope = base.package_scope
     _source = sources.PackageRepoSource
-    known_results = frozenset([
-        DeadMetadataUrl, RedirectedMetadataUrl, HttpsUrlAvailable, SSLCertificateError])
-    required_addons = (addons.UseAddon,)
-
-    def __init__(self, *args, use_addon, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.dead_result = DeadMetadataUrl
-        self.redirected_result = RedirectedMetadataUrl
 
     def _get_urls(self, pkg):
         try:
@@ -286,7 +257,7 @@ class MetadataUrlCheck(_UrlCheck):
             for x in tree.xpath(f'//upstream/{element}'):
                 # skip mailto URLs from bugs-to
                 if x.text and x.text.startswith(('http://', 'https://', 'ftp://')):
-                    yield x.text
+                    yield f'metadata.xml: {element}', x.text
 
     def schedule(self, pkgs, *args, **kwargs):
         super().schedule(pkgs[0], *args, **kwargs)
