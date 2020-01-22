dbfd6a93f2a103b13d7b188a586f0e9419d7f19c
Tim Harder <radhermit@gmail.com>
2020-01-22 07:10:44 -0700

    tests: drop keyword tests that assume generic results don't exist

--- tests/module/checks/test_all.py
+++ tests/module/checks/test_all.py
@@ -3,14 +3,11 @@ from snakeoil.cli import arghparse
 from pkgcheck import const
 from pkgcheck.checks import init_checks
 
-_known_keywords = set()
-
 
 def test_checks():
     """Scan through all public checks and verify various aspects."""
     for name, cls in const.CHECKS.items():
         assert cls.known_results, f"check class {name!r} doesn't define known results"
-        _known_keywords.update(cls.known_results)
 
 
 def test_check_scope(tool):
@@ -30,7 +27,4 @@ def test_check_scope(tool):
 def test_keywords():
     """Scan through all public result keywords and verify various aspects."""
     for name, cls in const.KEYWORDS.items():
-        assert cls in _known_keywords, f"result class {name!r} not used by any checks"
         assert cls.level is not None, f"result class {name!r} missing level"
-    # verify dynamically scanned keywords are equal to the known results set
-    assert set(const.KEYWORDS.values()) == _known_keywords
