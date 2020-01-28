9e0370a0f1c7eb8db1985f18fcacb7992b289193
Tim Harder <radhermit@gmail.com>
2020-01-28 10:26:38 -0700

    doc/conf.py: drop unnecessary py2 support

--- doc/conf.py
+++ doc/conf.py
@@ -13,8 +13,6 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-from __future__ import unicode_literals
-
 from importlib import import_module
 import os
 import sys
