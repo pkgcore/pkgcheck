d9a7e12483dadfa8e292fdabbb95601db0be6d71
Tim Harder <radhermit@gmail.com>
2020-01-31 19:12:04 -0700

    pkgcheck scan: use shorter value example for disabling cache usage

--- src/pkgcheck/scripts/pkgcheck.py
+++ src/pkgcheck/scripts/pkgcheck.py
@@ -189,8 +189,8 @@ main_options.add_argument(
         sign must be used, e.g. ``--cache=-git``, otherwise the disabled
         argument is treated as an option.
 
-        In order to disable all cache usage, it's easiest to use ``--cache
-        false`` instead of explicitly listing all disabled cache types.
+        In order to disable all cache usage, it's easiest to use ``--cache no``
+        instead of explicitly listing all disabled cache types.
 
         When disabled, no caches will be saved to disk and results requiring
         caches (e.g. git-related checks) will be skipped.
