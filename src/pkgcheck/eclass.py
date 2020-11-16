67ad4be86871653ecc829cc5f805d772aad57145
Tim Harder <radhermit@gmail.com>
2020-11-16 01:24:54 -0700

    eclass: update_cache(): simplify repo conditionals

--- src/pkgcheck/eclass.py
+++ src/pkgcheck/eclass.py
@@ -43,58 +43,58 @@ class EclassAddon(caches.CachedAddon):
     def update_cache(self, force=False):
         """Update related cache and push updates to disk."""
         if self.options.cache['eclass']:
-            for repo in self.repos:
-                if 'gentoo' in repo.aliases:
-                    cache_file = self.cache_file(repo)
-                    cache_eclasses = False
-                    eclasses = {}
+            repos = (r for r in self.repos if 'gentoo' in r.aliases)
+            for repo in repos:
+                cache_file = self.cache_file(repo)
+                cache_eclasses = False
+                eclasses = {}
 
-                    if not force:
-                        # try loading cached eclass data
-                        try:
-                            with open(cache_file, 'rb') as f:
-                                eclasses = pickle.load(f)
-                            if eclasses.version != self.cache.version:
-                                logger.debug('forcing eclass repo cache regen due to outdated version')
-                                os.remove(cache_file)
-                        except FileNotFoundError:
-                            pass
-                        except (AttributeError, EOFError, ImportError, IndexError) as e:
-                            logger.debug('forcing eclass cache regen: %s', e)
+                if not force:
+                    # try loading cached eclass data
+                    try:
+                        with open(cache_file, 'rb') as f:
+                            eclasses = pickle.load(f)
+                        if eclasses.version != self.cache.version:
+                            logger.debug('forcing eclass repo cache regen due to outdated version')
                             os.remove(cache_file)
+                    except FileNotFoundError:
+                        pass
+                    except (AttributeError, EOFError, ImportError, IndexError) as e:
+                        logger.debug('forcing eclass cache regen: %s', e)
+                        os.remove(cache_file)
 
-                    # check for eclass removals
-                    for name, eclass in list(eclasses.items()):
-                        if not os.path.exists(eclass.path):
-                            del eclasses[name]
-                            cache_eclasses = True
+                # check for eclass removals
+                for name, eclass in list(eclasses.items()):
+                    if not os.path.exists(eclass.path):
+                        del eclasses[name]
+                        cache_eclasses = True
 
-                    # padding for progress output
-                    padding = max(len(x) for x in repo.eclass_cache.eclasses)
+                # padding for progress output
+                padding = max(len(x) for x in repo.eclass_cache.eclasses)
 
-                    # check for eclass additions and updates
-                    with base.ProgressManager(verbosity=self.options.verbosity) as progress:
-                        for name, eclass in sorted(repo.eclass_cache.eclasses.items()):
-                            try:
-                                if os.path.getmtime(eclass.path) != eclasses[name].mtime:
-                                    raise KeyError
-                            except (KeyError, AttributeError):
-                                try:
-                                    progress(f'updating eclass cache: {name:<{padding}}')
-                                    eclasses[name] = Eclass(eclass.path, sourced=True)
-                                    cache_eclasses = True
-                                except (IOError, EclassDocParsingError):
-                                    continue
-
-                    # push eclasses to disk if any changes were found
-                    if cache_eclasses:
+                # check for eclass additions and updates
+                with base.ProgressManager(verbosity=self.options.verbosity) as progress:
+                    for name, eclass in sorted(repo.eclass_cache.eclasses.items()):
                         try:
-                            os.makedirs(os.path.dirname(cache_file), exist_ok=True)
-                            f = AtomicWriteFile(cache_file, binary=True)
-                            f.write(pickle.dumps(_EclassCache(eclasses)))
-                            f.close()
-                        except IOError as e:
-                            msg = f'failed dumping eclasses: {cache_file!r}: {e.strerror}'
-                            raise UserException(msg)
+                            if os.path.getmtime(eclass.path) != eclasses[name].mtime:
+                                raise KeyError
+                        except (KeyError, AttributeError):
+                            try:
+                                progress(f'updating eclass cache: {name:<{padding}}')
+                                eclasses[name] = Eclass(eclass.path, sourced=True)
+                                cache_eclasses = True
+                            except (IOError, EclassDocParsingError):
+                                continue
 
-                    self.eclasses = eclasses
+                # push eclasses to disk if any changes were found
+                if cache_eclasses:
+                    try:
+                        os.makedirs(os.path.dirname(cache_file), exist_ok=True)
+                        f = AtomicWriteFile(cache_file, binary=True)
+                        f.write(pickle.dumps(_EclassCache(eclasses)))
+                        f.close()
+                    except IOError as e:
+                        msg = f'failed dumping eclasses: {cache_file!r}: {e.strerror}'
+                        raise UserException(msg)
+
+                self.eclasses = eclasses
