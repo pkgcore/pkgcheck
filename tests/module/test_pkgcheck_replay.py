3845c1279df19cf3407f8f6cfa1fe89b510016c3
Tim Harder <radhermit@gmail.com>
2020-11-21 06:07:50 -0700

    tests: split `pkgcheck replay` into a separate test module

---
+++ tests/module/test_pkgcheck_replay.py
@@ -0,0 +1,46 @@
+import tempfile
+from functools import partial
+from unittest.mock import patch
+
+import pytest
+from pkgcheck import __title__ as project
+from pkgcheck import reporters
+from pkgcheck.checks.profiles import ProfileWarning
+from pkgcheck.scripts import run
+from snakeoil.formatters import PlainTextFormatter
+
+
+class TestPkgcheckReplay:
+
+    script = partial(run, project)
+
+    @pytest.fixture(autouse=True)
+    def _setup(self, fakeconfig):
+        self.args = [project, '--config', fakeconfig, 'replay']
+
+    def test_missing_file_arg(self, capsys):
+        with patch('sys.argv', self.args):
+            with pytest.raises(SystemExit) as excinfo:
+                self.script()
+            out, err = capsys.readouterr()
+            assert not out
+            err = err.strip().split('\n')
+            assert len(err) == 1
+            assert err[0] == (
+                'pkgcheck replay: error: the following arguments are required: FILE')
+            assert excinfo.value.code == 2
+
+    def test_replay(self, capsys):
+        result = ProfileWarning('profile warning: foo')
+        for reporter_cls in (reporters.BinaryPickleStream, reporters.JsonStream):
+            with tempfile.NamedTemporaryFile() as f:
+                out = PlainTextFormatter(f)
+                with reporter_cls(out) as reporter:
+                    reporter.report(result)
+                with patch('sys.argv', self.args + ['-R', 'StrReporter', f.name]):
+                    with pytest.raises(SystemExit) as excinfo:
+                        self.script()
+                    out, err = capsys.readouterr()
+                    assert not err
+                    assert out == 'profile warning: foo\n'
+                    assert excinfo.value.code == 0
