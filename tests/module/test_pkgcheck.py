f876863f6257246d267e0104c3b67f5f4063528b
Tim Harder <radhermit@gmail.com>
2020-11-21 04:36:55 -0700

    tests: add explicit `pkgcheck cache` forced removal test

--- tests/module/test_pkgcheck.py
+++ tests/module/test_pkgcheck.py
@@ -609,6 +609,41 @@ class TestPkgcheckCache:
                 assert (out, err) == ('', '')
                 assert excinfo.value.code == 0
 
+    def test_cache_forced_removal(self, capsys):
+        with patch('pkgcheck.const.USER_CACHE_DIR', self.cache_dir):
+            # force stubrepo profiles cache regen
+            with patch('sys.argv', self.args + ['-uf']):
+                with pytest.raises(SystemExit):
+                    self.script()
+
+            # fail to forcibly remove all
+            with patch('pkgcheck.caches.shutil.rmtree') as rmtree, \
+                    patch('sys.argv', self.args + ['-rf']):
+                rmtree.side_effect = IOError('bad perms')
+                with pytest.raises(SystemExit) as excinfo:
+                    self.script()
+                out, err = capsys.readouterr()
+                assert not out
+                assert err.strip() == 'pkgcheck cache: error: failed removing cache dir: bad perms'
+                assert excinfo.value.code == 2
+
+            # actually forcibly remove all
+            with patch('sys.argv', self.args + ['-rf']):
+                rmtree.side_effect = IOError('bad perms')
+                with pytest.raises(SystemExit) as excinfo:
+                    self.script()
+                out, err = capsys.readouterr()
+                assert (out, err) == ('', '')
+                assert excinfo.value.code == 0
+
+            # verify it's gone
+            with patch('sys.argv', self.args):
+                with pytest.raises(SystemExit) as excinfo:
+                    self.script()
+                out, err = capsys.readouterr()
+                assert (out, err) == ('', '')
+                assert excinfo.value.code == 0
+
 
 class TestPkgcheckShow:
 
