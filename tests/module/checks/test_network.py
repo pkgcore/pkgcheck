3b9048da86a43c6b7e0da8b1d823d71e321699d6
Tim Harder <radhermit@gmail.com>
2020-12-20 16:29:52 -0700

    tests: use scan() API call instead of calling script

--- tests/module/checks/test_network.py
+++ tests/module/checks/test_network.py
@@ -1,6 +1,5 @@
 import glob
 import importlib.util
-import io
 import os
 import socket
 import tempfile
@@ -9,12 +8,10 @@ from functools import partial
 from operator import attrgetter
 from unittest.mock import patch
 
-from pkgcheck import __title__ as project
-from pkgcheck import objects, reporters
+from pkgcheck import objects, reporters, scan
 from pkgcheck.checks import NetworkCheck
 from pkgcheck.checks.network import HomepageUrlCheck, FetchablesUrlCheck, DeadUrl
 from pkgcheck.packages import RawCPV
-from pkgcheck.scripts import run
 import pytest
 from snakeoil.formatters import PlainTextFormatter
 from snakeoil.osutils import pjoin
@@ -24,17 +21,16 @@ requests = pytest.importorskip('requests')
 
 class TestNetworkChecks:
 
-    script = partial(run, project)
     testdir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
     repos_data = pjoin(testdir, 'data', 'repos')
     repos_dir = pjoin(testdir, 'repos')
 
     @pytest.fixture(autouse=True)
     def _setup(self, testconfig, tmp_path):
-        self.cache_dir = str(tmp_path)
-        self.args = [
-            project, '--config', testconfig,
-            'scan', '--config', 'no', '--cache', 'no', '--net',
+        base_args = ['--config', testconfig]
+        self.scan = partial(scan, base_args=base_args)
+        self.scan_args = [
+            '--config', 'no', '--cache', 'no', '--net',
             '-r', pjoin(self.repos_dir, 'network'),
         ]
 
@@ -55,7 +51,7 @@ class TestNetworkChecks:
             return output
 
     @pytest.mark.parametrize('check, result', _net_results)
-    def test_scan(self, check, result, capsys):
+    def test_scan(self, check, result):
         check_name = check.__name__
         keyword = result.__name__
         result_dir = pjoin(self.repos_dir, 'network', check_name, keyword)
@@ -75,43 +71,31 @@ class TestNetworkChecks:
             spec.loader.exec_module(responses_mod)
 
             results = []
-            args = [
-                '-R' 'JsonStream', '-c', check_name, '-k', keyword,
-                f'{check_name}/{ebuild_name}'
-            ]
+            args = ['-c', check_name, '-k', keyword, f'{check_name}/{ebuild_name}']
             with patch('pkgcheck.net.requests.Session.send') as send:
-                with patch('sys.argv', self.args + args):
-                    send.side_effect = responses_mod.responses
-                    with pytest.raises(SystemExit) as excinfo:
-                        self.script()
-                    assert excinfo.value.code == 0
+                send.side_effect = responses_mod.responses
 
-                    # load expected results if they exist
-                    try:
-                        with open(pjoin(data_dir, 'expected.json')) as f:
-                            expected_results = set(reporters.JsonStream.from_iter(f))
-                    except FileNotFoundError:
-                        # check stopped before making request or completed successfully
-                        continue
+                # load expected results if they exist
+                try:
+                    with open(pjoin(data_dir, 'expected.json')) as f:
+                        expected_results = set(reporters.JsonStream.from_iter(f))
+                except FileNotFoundError:
+                    # check stopped before making request or completed successfully
+                    continue
 
-                    out, err = capsys.readouterr()
-                    assert out, 'no results exist'
-                    for result in reporters.JsonStream.from_iter(io.StringIO(out)):
-                        results.append(result)
-
-                    assert expected_results, 'regular results must always exist'
-                    rendered_results = self._render_results(results)
-                    assert rendered_results, 'failed rendering results'
-                    if set(results) != expected_results:
-                        error = ['unmatched results:']
-                        expected = self._render_results(expected_results)
-                        error.append(f'expected:\n{expected}')
-                        error.append(f'got:\n{rendered_results}')
-                        pytest.fail('\n'.join(error))
+                results = list(self.scan(self.scan_args + args))
+                rendered_results = self._render_results(results)
+                assert rendered_results, 'failed rendering results'
+                if set(results) != expected_results:
+                    error = ['unmatched results:']
+                    expected = self._render_results(expected_results)
+                    error.append(f'expected:\n{expected}')
+                    error.append(f'got:\n{rendered_results}')
+                    pytest.fail('\n'.join(error))
 
     @pytest.mark.parametrize(
         'check, result', ((HomepageUrlCheck, DeadUrl), (FetchablesUrlCheck, DeadUrl)))
-    def test_scan_ftp(self, check, result, capsys):
+    def test_scan_ftp(self, check, result):
         check_name = check.__name__
         keyword = result.__name__
 
@@ -127,27 +111,15 @@ class TestNetworkChecks:
             (None, None),  # faking a clean connection
         )
 
-        args = [
-            '-R', 'JsonStream', '-c', check_name, '-k', keyword,
-            f'{check_name}/ftp-{keyword}'
-        ]
+        args = ['-c', check_name, '-k', keyword, f'{check_name}/ftp-{keyword}']
         for side_effect, expected_result in data:
             with patch('pkgcheck.checks.network.urllib.request.urlopen') as urlopen:
-                with patch('sys.argv', self.args + args):
-                    if side_effect is not None:
-                        urlopen.side_effect = side_effect
-                    with pytest.raises(SystemExit) as excinfo:
-                        self.script()
-                    out, err = capsys.readouterr()
-                    assert not err
-                    if side_effect is None:
-                        assert not out
-                    else:
-                        assert out, 'no results exist'
-                        assert excinfo.value.code == 0
-                        results = []
-                        for result in reporters.JsonStream.from_iter(io.StringIO(out)):
-                            results.append(result)
-                        assert len(results) == 1
-                        assert results[0] == expected_result
-                        assert self._render_results(results), 'failed rendering results'
+                if side_effect is not None:
+                    urlopen.side_effect = side_effect
+                results = list(self.scan(self.scan_args + args))
+                if side_effect is None:
+                    assert not results
+                else:
+                    assert len(results) == 1
+                    assert results[0] == expected_result
+                    assert self._render_results(results), 'failed rendering results'
