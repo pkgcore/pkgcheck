cd39433946044389a81b2f48f13e8cea2e5e5a0f
Tim Harder <radhermit@gmail.com>
2021-03-02 13:40:21 -0700

    pkgcheck: make snakeoil register known subcommands internally

--- src/pkgcheck/scripts/pkgcheck.py
+++ src/pkgcheck/scripts/pkgcheck.py
@@ -7,11 +7,4 @@ ebuild repositories for various issues.
 from pkgcore.util import commandline
 
 argparser = commandline.ArgumentParser(
-    description=__doc__, help=False, script=(__file__, __name__))
-
-subparsers = argparser.add_subparsers()
-subparsers.add_command('scan')
-subparsers.add_command('cache')
-subparsers.add_command('ci')
-subparsers.add_command('replay')
-subparsers.add_command('show')
+    description=__doc__, help=False, subcmds=True, script=(__file__, __name__))
