6e440f0806b0707dbf7265c7fac74944881af8cc
Tim Harder <radhermit@gmail.com>
2021-03-04 02:10:37 -0700

    data: use newline-separated format for GentooCI keyword list

--- data/ci.sh
+++ data/ci.sh
@@ -2,6 +2,11 @@
 # Output the current list of Gentoo CI keyword errors.
 # Requires bash, curl, and jq installed.
 
+# pull latest CI keyword errors
 keywords=$(curl -s https://raw.githubusercontent.com/mgorny/pkgcheck2html/master/pkgcheck2html.conf.json | jq -r 'to_entries | .[] | select(.value == "err") | .key' | sort)
-errors=$(printf '%s,' ${keywords[@]})
-echo ${errors%,}
+
+# replace checkset in bundled config
+cat << EOF > pkgcheck.conf
+[CHECKSETS]
+GentooCI =$(printf '\n  %s' ${keywords[@]})
+EOF
