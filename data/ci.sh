f048ff739d2e2e374e6f323b3a016355db05bdb3
Tim Harder <radhermit@gmail.com>
2021-03-04 02:27:03 -0700

    data/ci.sh: move json url into a separate variable

--- data/ci.sh
+++ data/ci.sh
@@ -3,7 +3,8 @@
 # Requires bash, curl, and jq installed.
 
 # pull latest CI keyword errors
-keywords=$(curl -s https://raw.githubusercontent.com/mgorny/pkgcheck2html/master/pkgcheck2html.conf.json | jq -r 'to_entries | .[] | select(.value == "err") | .key' | sort)
+JSON_URL="https://raw.githubusercontent.com/mgorny/pkgcheck2html/master/pkgcheck2html.conf.json"
+keywords=$(curl -s ${JSON_URL} | jq -r 'to_entries | .[] | select(.value == "err") | .key' | sort)
 
 # replace checkset in bundled config
 cat << EOF > pkgcheck.conf
