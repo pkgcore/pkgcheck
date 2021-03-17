23f7bc2e4547d06bfedc6f3ff04d436ee14e9e5f
Tim Harder <radhermit@gmail.com>
2021-03-17 14:19:45 -0600

    tests: add initial Scope object tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,7 +1,34 @@
 from itertools import chain
 from unittest.mock import patch
 
+from pkgcheck import base
 from pkgcheck.base import ProgressManager
+
+
+class TestScope:
+
+    def test_rich_comparisons(self):
+        assert base.commit_scope < base.repo_scope
+        assert base.commit_scope < 0
+        assert base.commit_scope <= base.repo_scope
+        assert base.commit_scope <= 0
+        assert base.repo_scope > base.commit_scope
+        assert base.repo_scope > 0
+        assert base.repo_scope >= base.commit_scope
+        assert base.repo_scope >= 0
+        assert base.repo_scope == base.repo_scope
+        assert base.repo_scope == 1
+        assert base.repo_scope != base.commit_scope
+        assert base.repo_scope != 0
+
+    def test_hash(self):
+        assert base.repo_scope in {base.repo_scope, base.commit_scope}
+
+    def test_repr(self):
+        assert base.repo_scope.desc in repr(base.repo_scope)
+
+    def test_str(self):
+        assert base.repo_scope.desc in str(base.repo_scope)
 
 
 class TestProgressManager:
