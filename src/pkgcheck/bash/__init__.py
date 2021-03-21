e164c2cbf03afc94b5f68cf20f44878d440638d5
Tim Harder <radhermit@gmail.com>
2021-03-21 08:07:08 -0600

    move ParseTree from sources to bash module

--- src/pkgcheck/bash/__init__.py
+++ src/pkgcheck/bash/__init__.py
@@ -104,3 +104,32 @@ if os.path.exists(lib):
     func_query = query('(function_definition) @func')
     var_assign_query = query('(variable_assignment) @assign')
     var_query = query('(variable_name) @var')
+
+
+class ParseTree:
+    """Bash parse tree object and support."""
+
+    def __init__(self, data, **kwargs):
+        super().__init__(**kwargs)
+        self.data = data
+        self.tree = parser.parse(data)
+
+    def node_str(self, node):
+        """Return the ebuild string associated with a given parse tree node."""
+        return self.data[node.start_byte:node.end_byte].decode('utf8')
+
+    def global_query(self, query):
+        """Run a given parse tree query returning only those nodes in global scope."""
+        for x in self.tree.root_node.children:
+            # skip nodes in function scope
+            if x.type != 'function_definition':
+                for node, _ in query.captures(x):
+                    yield node
+
+    def func_query(self, query):
+        """Run a given parse tree query returning only those nodes in function scope."""
+        for x in self.tree.root_node.children:
+            # only return nodes in function scope
+            if x.type == 'function_definition':
+                for node, _ in query.captures(x):
+                    yield node
--- src/pkgcheck/sources.py
+++ src/pkgcheck/sources.py
@@ -11,7 +11,8 @@ from pkgcore.ebuild.repository import UnconfiguredTree
 from pkgcore.restrictions import packages
 from snakeoil.osutils import listdir_files, pjoin
 
-from . import addons, base, bash
+from . import addons, base
+from .bash import ParseTree
 from .addons.eclass import Eclass, EclassAddon
 from .addons.profiles import ProfileAddon, ProfileNode
 from .packages import FilteredPkg, RawCPV, WrappedPkg
@@ -289,35 +290,6 @@ class EbuildFileRepoSource(RepoSource):
     def itermatch(self, restrict, **kwargs):
         for pkg in super().itermatch(restrict, **kwargs):
             yield _SourcePkg(pkg)
-
-
-class ParseTree:
-    """Bash parse tree object and support."""
-
-    def __init__(self, data, **kwargs):
-        super().__init__(**kwargs)
-        self.data = data
-        self.tree = bash.parser.parse(data)
-
-    def node_str(self, node):
-        """Return the ebuild string associated with a given parse tree node."""
-        return self.data[node.start_byte:node.end_byte].decode('utf8')
-
-    def global_query(self, query):
-        """Run a given parse tree query returning only those nodes in global scope."""
-        for x in self.tree.root_node.children:
-            # skip nodes in function scope
-            if x.type != 'function_definition':
-                for node, _ in query.captures(x):
-                    yield node
-
-    def func_query(self, query):
-        """Run a given parse tree query returning only those nodes in function scope."""
-        for x in self.tree.root_node.children:
-            # only return nodes in function scope
-            if x.type == 'function_definition':
-                for node, _ in query.captures(x):
-                    yield node
 
 
 class _ParsedPkg(ParseTree, WrappedPkg):
