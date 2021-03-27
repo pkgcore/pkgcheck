24271fb671e1a8a49006d69ca52ccad99dcff71c
Tim Harder <radhermit@gmail.com>
2021-03-27 11:21:09 -0600

    bump version

--- src/pkgcheck/__init__.py
+++ src/pkgcheck/__init__.py
@@ -6,7 +6,7 @@ from .results import Result
 
 __all__ = ('keywords', 'scan', 'PkgcheckException', 'Result')
 __title__ = 'pkgcheck'
-__version__ = '0.9.7'
+__version__ = '0.9.8'
 
 
 def __getattr__(name):
