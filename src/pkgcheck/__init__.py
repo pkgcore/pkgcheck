3f6b74aa1aac79bdba24032b46522e78efb84965
Michał Górny <mgorny@gentoo.org>
2021-08-16 10:43:46 +0200

    bump version

--- src/pkgcheck/__init__.py
+++ src/pkgcheck/__init__.py
@@ -6,7 +6,7 @@ from .results import Result
 
 __all__ = ('keywords', 'scan', 'PkgcheckException', 'Result')
 __title__ = 'pkgcheck'
-__version__ = '0.10.5'
+__version__ = '0.10.6'
 
 
 def __getattr__(name):
