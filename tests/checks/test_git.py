pkgcore/pkgcheck

commit 9ed3caaf86adf1141d9085683b37c5030eded2f2

GitPkgCommitsCheck: fix files check
When using _RemovalRepo for previous git state repo, it uses
UnconfiguredTree::_pkg_filter as default pkg_filter. During the
check for various bad file, it tries to parse the ebuild, which in
turn tries to check the usage around inherited eclasses - which
doesn't exist in this repo.

In theory, the setting of masters repo as the original one should
suffice, but it doesn't in practice. For now, let's copy the eclass
content so at least this very important check is done!

Fixes: https://github.com/pkgcore/pkgcheck/issues/381
Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

src/pkgcheck/checks/git.py
@@ -2,6 +2,7 @@
 
 import os
 import re
+import shutil
 import subprocess
 import tarfile
 from collections import defaultdict
@@ -187,6 +188,8 @@ class _RemovalRepo(UnconfiguredTree):
         os.makedirs(pjoin(repo_dir, 'profiles'))
         with open(pjoin(repo_dir, 'profiles', 'repo_name'), 'w') as f:
             f.write('old-repo\n')
+        if os.path.exists(pjoin(repo_dir, 'eclass')):
+            shutil.copytree(pjoin(repo.location, 'eclass'), pjoin(repo_dir, 'eclass'), dirs_exist_ok=True)
         super().__init__(repo_dir)
 
     def __call__(self, pkgs):

commit e12f045393dfa27b2730caae844f50c38f6afadb

GitPkgCommitsCheck: fix the eclass copying for RemovalRepo
Idea was suggested by radhermit. I have also added a test which
fails in original situation and passes now.

Fixes: 9ed3caaf86adf1141d9085683b37c5030eded2f2
Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

src/pkgcheck/checks/git.py
@@ -2,7 +2,6 @@
 
 import os
 import re
-import shutil
 import subprocess
 import tarfile
 from collections import defaultdict
@@ -188,8 +187,6 @@ class _RemovalRepo(UnconfiguredTree):
         os.makedirs(pjoin(repo_dir, 'profiles'))
         with open(pjoin(repo_dir, 'profiles', 'repo_name'), 'w') as f:
             f.write('old-repo\n')
-        if os.path.exists(pjoin(repo_dir, 'eclass')):
-            shutil.copytree(pjoin(repo.location, 'eclass'), pjoin(repo_dir, 'eclass'), dirs_exist_ok=True)
         super().__init__(repo_dir)
 
     def __call__(self, pkgs):
@@ -206,6 +203,8 @@ class _RemovalRepo(UnconfiguredTree):
         """Populate the repo with a given sequence of historical packages."""
         pkg = pkgs[0]
         paths = [pjoin(pkg.category, pkg.package)]
+        if os.path.exists(pjoin(self.__parent_repo.location, 'eclass')):
+            paths.append('eclass')
         old_files = subprocess.Popen(
             ['git', 'archive', f'{pkg.commit}~1'] + paths,
             stdout=subprocess.PIPE, stderr=subprocess.PIPE,

tests/checks/test_git.py
@@ -466,6 +466,22 @@ class TestGitPkgCommitsCheck(ReportTestCase):
         expected = git_mod.DroppedUnstableKeywords(['~amd64'], commit, pkg=CPV('cat/pkg-1'))
         assert r == expected
 
+    def test_dropped_keywords_inherit_eclass(self):
+        # add stable ebuild to parent repo
+        with open(pjoin(self.parent_git_repo.path, 'eclass/make.eclass'), 'w') as f:
+            f.write(':')
+        self.parent_git_repo.add_all('make.eclass: initial commit')
+        self.parent_repo.create_ebuild('cat/pkg-1', keywords=['~amd64'], data="inherit make")
+        self.parent_git_repo.add_all('cat/pkg: version bump to 1')
+        # pull changes and remove it from the child repo
+        self.child_git_repo.run(['git', 'pull', 'origin', 'main'])
+        self.child_git_repo.remove('cat/pkg/pkg-1.ebuild', msg='cat/pkg: remove 1')
+        commit = self.child_git_repo.HEAD
+        self.init_check()
+        r = self.assertReport(self.check, self.source)
+        expected = git_mod.DroppedUnstableKeywords(['~amd64'], commit, pkg=CPV('cat/pkg-1'))
+        assert r == expected
+
     def test_rdepend_change(self):
         # add pkgs to parent repo
         self.parent_repo.create_ebuild('cat/dep1-0')