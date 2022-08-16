37100f0c63dcd1522d3f36507bdd1c6563b27609
Arthur Zamarin <arthurzam@gentoo.org>
2022-08-16 20:15:40 +0300

    ci: fix release testing on musl
    Follows: bfbf6b7b1abd4f07e1409889f14b1aff5a1ed2de
    Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

--- .github/workflows/manylinux.sh
+++ .github/workflows/manylinux.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 # Mangle the manylinux docker image to successfully build and test wheels.
 
+set -e
+
 # install git
-apt-get update
-apt-get install -y git
+if type -P apk; then
+    apk add --no-cache git bash
+else
+    apt-get update
+    apt-get install -y git
+fi
 
 # download static build of recent bash release
 URL="https://github.com/robxu9/bash-static/releases/download/5.1.008-1.2.2/bash-linux-x86_64"
