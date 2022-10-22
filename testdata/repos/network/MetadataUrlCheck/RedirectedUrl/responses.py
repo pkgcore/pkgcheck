807e6229e5ebc20f8372d053af47577dee74ccd2
Arthur Zamarin <arthurzam@gentoo.org>
2022-10-22 11:55:11 +0300

    tests: fix flaky network scan MetadataUrlCheck/RedirectedUrl
    Because of undefined order between requests call by the check (as they
    are performed in parallel), the test could fail if there was no match
    with the expected requests replies.
    
    Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

--- testdata/repos/network/MetadataUrlCheck/RedirectedUrl/responses.py
+++ testdata/repos/network/MetadataUrlCheck/RedirectedUrl/responses.py
@@ -1,21 +1,6 @@
 import io
 
 from requests.models import Response
-
-r_hist = Response()
-r_hist.status_code = 301
-r_hist.reason = 'Moved Permanently'
-r_hist.url = 'https://github.com/pkgcore/pkgcheck/changelog'
-r_hist.headers = {'location': 'https://github.com/pkgcore/pkgcheck/news'}
-r_hist.raw = io.StringIO()
-
-r1 = Response()
-r1.status_code = 200
-r1.reason = 'OK'
-r1.url = 'https://github.com/pkgcore/pkgcheck/changelog'
-r1.raw = io.StringIO()
-r1.history = [r_hist]
-
 r_hist = Response()
 r_hist.status_code = 301
 r_hist.reason = 'Moved Permanently'
@@ -23,11 +8,11 @@ r_hist.url = 'https://github.com/pkgcore/pkgcheck'
 r_hist.headers = {'location': 'https://github.com/pkgcore/pkgcheck/'}
 r_hist.raw = io.StringIO()
 
-r2 = Response()
-r2.status_code = 301
-r2.reason = 'OK'
-r2.url = 'https://github.com/pkgcore/pkgcheck'
-r2.raw = io.StringIO()
-r2.history = [r_hist]
+r = Response()
+r.status_code = 301
+r.reason = 'OK'
+r.url = 'https://github.com/pkgcore/pkgcheck'
+r.raw = io.StringIO()
+r.history = [r_hist]
 
-responses = [r1, r2]
+responses = [r]
