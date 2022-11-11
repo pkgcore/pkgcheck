cfc39a9f7d011b1fa8fe26ac937a0ac5ff1cf6e7
Arthur Zamarin <arthurzam@gentoo.org>
2022-11-11 18:14:40 +0200

    start work on 0.10.19
    Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

--- src/pkgcheck/__init__.py
+++ src/pkgcheck/__init__.py
@@ -6,7 +6,7 @@ from .results import Result
 
 __all__ = ('keywords', 'scan', 'PkgcheckException', 'Result')
 __title__ = 'pkgcheck'
-__version__ = '0.10.18'
+__version__ = '0.10.19'
 
 
 def __getattr__(name):
