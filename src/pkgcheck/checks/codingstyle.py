pkgcore/pkgcheck

commit 2cfc92ccab6ddfedd79ec16b8ebdde4eae8d2ad4

VariableScopeCheck: also check for wrong usage on global scope
Resolves: https://github.com/pkgcore/pkgcheck/issues/606
Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

src/pkgcheck/checks/codingstyle.py
@@ -72,7 +72,7 @@ class BadCommandsCheck(Check):
             for node, _ in bash.cmd_query.captures(func_node):
                 call = pkg.node_str(node)
                 name = pkg.node_str(node.child_by_field_name("name"))
-                lineno, colno = node.start_point
+                lineno, _colno = node.start_point
                 if name in pkg.eapi.bash_cmds_banned:
                     yield BannedEapiCommand(
                         name, line=call, lineno=lineno + 1, eapi=pkg.eapi, pkg=pkg
@@ -942,7 +942,7 @@ class ReadonlyVariableCheck(Check):
             name = pkg.node_str(node.child_by_field_name("name"))
             if name in self.readonly_vars:
                 call = pkg.node_str(node)
-                lineno, colno = node.start_point
+                lineno, _colno = node.start_point
                 yield ReadonlyVariable(name, line=call, lineno=lineno + 1, pkg=pkg)
 
 
@@ -969,7 +969,7 @@ class VariableScopeCheck(Check):
     """Scan ebuilds for variables that are only allowed in certain scopes."""
 
     _source = sources.EbuildParseRepoSource
-    known_results = frozenset([EbuildVariableScope])
+    known_results = frozenset({EbuildVariableScope})
 
     # see https://projects.gentoo.org/pms/7/pms.html#x1-10900011.1
     variable_map = ImmutableDict(
@@ -987,13 +987,34 @@ class VariableScopeCheck(Check):
             "SYSROOT": ("src_", "pkg_setup"),
             "ESYSROOT": ("src_", "pkg_setup"),
             "BROOT": ("src_", "pkg_setup"),
-            "D": ("src_install", "pkg_preinst"),
-            "ED": ("src_install", "pkg_preinst"),
+            "D": ("src_install", "pkg_preinst", "pkg_postinst"),
+            "ED": ("src_install", "pkg_preinst", "pkg_postinst"),
             "DESTTREE": "src_install",
             "INSDESTTREE": "src_install",
             "MERGE_TYPE": "pkg_",
             "REPLACING_VERSIONS": "pkg_",
             "REPLACED_BY_VERSION": ("pkg_prerm", "pkg_postrm"),
+        }
+    )
+
+    not_global_scope = frozenset(
+        {
+            "A",
+            "AA",
+            "BROOT",
+            "D",
+            "DESTTREE",
+            "ECLASSDIR",
+            "ED",
+            "EROOT",
+            "ESYSROOT",
+            "INSDESTTREE",
+            "MERGE_TYPE",
+            "PORTDIR",
+            "REPLACED_BY_VERSION",
+            "REPLACING_VERSIONS",
+            "ROOT",
+            "SYSROOT",
         }
     )
 
@@ -1014,10 +1035,21 @@ class VariableScopeCheck(Check):
                 for var_node, _ in bash.var_query.captures(func_node):
                     var_name = pkg.node_str(var_node)
                     if var_name in variables:
-                        lineno, colno = var_node.start_point
+                        lineno, _colno = var_node.start_point
                         usage[var_name].add(lineno + 1)
                 for var, lines in sorted(usage.items()):
                     yield EbuildVariableScope(var, func_name, lines=sorted(lines), pkg=pkg)
+
+        global_usage = defaultdict(set)
+        for global_node in pkg.tree.root_node.children:
+            if global_node.type not in ("function_definition", "ERROR"):
+                for var_node, _ in bash.var_query.captures(global_node):
+                    var_name = pkg.node_str(var_node)
+                    if var_name in self.not_global_scope:
+                        lineno, _colno = var_node.start_point
+                        global_usage[var_name].add(lineno + 1)
+        for var, lines in sorted(global_usage.items()):
+            yield EbuildVariableScope(var, "global scope", lines=sorted(lines), pkg=pkg)
 
 
 class RedundantDodir(results.LineResult, results.Style):

testdata/data/repos/standalone/VariableScopeCheck/EbuildVariableScope/expected.json
@@ -1,1 +1,2 @@
-{"__class__": "EbuildVariableScope", "category": "VariableScopeCheck", "package": "EbuildVariableScope", "version": "0", "variable": "EROOT", "func": "src_configure", "lines": [10]}
+{"__class__": "EbuildVariableScope", "category": "VariableScopeCheck", "package": "EbuildVariableScope", "version": "0", "lines": [12], "variable": "EROOT", "func": "src_configure"}
+{"__class__": "EbuildVariableScope", "category": "VariableScopeCheck", "package": "EbuildVariableScope", "version": "0", "lines": [8], "variable": "ROOT", "func": "global scope"}

testdata/data/repos/standalone/VariableScopeCheck/EbuildVariableScope/fix.patch
@@ -1,8 +1,12 @@
 diff -Naur standalone/VariableScopeCheck/EbuildVariableScope/EbuildVariableScope-0.ebuild fixed/VariableScopeCheck/EbuildVariableScope/EbuildVariableScope-0.ebuild
 --- standalone/VariableScopeCheck/EbuildVariableScope/EbuildVariableScope-0.ebuild	2021-03-17 01:20:05.423678951 -0600
 +++ fixed/VariableScopeCheck/EbuildVariableScope/EbuildVariableScope-0.ebuild	2021-03-17 01:21:57.421132212 -0600
-@@ -7,7 +7,7 @@
+@@ -5,11 +5,9 @@ HOMEPAGE="https://github.com/pkgcore/pkgcheck"
+ SLOT="0"
+ LICENSE="BSD"
 
+-DOC_CONTENTS="Hello ${ROOT}"
+-
  src_configure() {
  	# EROOT isn't allowed in src_* phases
 -	econf --sysconfdir="${EROOT}/etc/${PN}"

testdata/repos/standalone/VariableScopeCheck/EbuildVariableScope/EbuildVariableScope-0.ebuild
@@ -5,6 +5,8 @@ HOMEPAGE="https://github.com/pkgcore/pkgcheck"
 SLOT="0"
 LICENSE="BSD"
 
+DOC_CONTENTS="Hello ${ROOT}"
+
 src_configure() {
 	# EROOT isn't allowed in src_* phases
 	econf --sysconfdir="${EROOT}/etc/${PN}"

commit a327dcfb96db1aba2499bdf8d84a298fa815adc5

VariableScopeCheck: Disallow D and ED in pkg_postinst (again)
https://projects.gentoo.org/qa/policy-guide/ebuild-format.html#pg0107

Fixes: 2cfc92ccab6ddfedd79ec16b8ebdde4eae8d2ad4
Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

src/pkgcheck/checks/codingstyle.py
@@ -987,8 +987,8 @@ class VariableScopeCheck(Check):
             "SYSROOT": ("src_", "pkg_setup"),
             "ESYSROOT": ("src_", "pkg_setup"),
             "BROOT": ("src_", "pkg_setup"),
-            "D": ("src_install", "pkg_preinst", "pkg_postinst"),
-            "ED": ("src_install", "pkg_preinst", "pkg_postinst"),
+            "D": ("src_install", "pkg_preinst"),  # pkg_postinst is forbidden by QA policy PG 107
+            "ED": ("src_install", "pkg_preinst"),  # pkg_postinst is forbidden by QA policy PG 107
             "DESTTREE": "src_install",
             "INSDESTTREE": "src_install",
             "MERGE_TYPE": "pkg_",