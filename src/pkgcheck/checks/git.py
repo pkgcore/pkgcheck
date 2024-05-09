pkgcore/pkgcheck

commit 4a7a02f60c9f87ad85670e0e798c5aadb8096c92

NewerEAPIAvailable: handle better when no eclasses EAPI
It might sometimes not find any intersection of eclasses EAPIs, in which
it is better to not report anything then to report a false positive or
worse, to explode with exception.

Resolves: https://github.com/pkgcore/pkgcheck/issues/679
Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

src/pkgcheck/checks/git.py
@@ -388,7 +388,7 @@ class GitPkgCommitsCheck(GentooRepoCheck, GitCommitsCheck):
                 for eclass in new_pkg.inherit
             )
             current_eapi = int(str(new_pkg.eapi))
-            common_max_eapi = max(frozenset.intersection(*eclass_eapis))
+            common_max_eapi = max(frozenset.intersection(*eclass_eapis), 0)
             if common_max_eapi > current_eapi:
                 yield NewerEAPIAvailable(common_max_eapi, pkg=new_pkg)
 

commit 4f08057161483e92cd64febac44ffbc1dd3bc718

NewerEAPIAvailable: fix code
Fixes: 4a7a02f60c9f87ad85670e0e798c5aadb8096c92
Resolves: https://github.com/pkgcore/pkgcheck/issues/679
Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

src/pkgcheck/checks/git.py
@@ -388,7 +388,7 @@ class GitPkgCommitsCheck(GentooRepoCheck, GitCommitsCheck):
                 for eclass in new_pkg.inherit
             )
             current_eapi = int(str(new_pkg.eapi))
-            common_max_eapi = max(frozenset.intersection(*eclass_eapis), 0)
+            common_max_eapi = max(frozenset.intersection(*eclass_eapis), default=0)
             if common_max_eapi > current_eapi:
                 yield NewerEAPIAvailable(common_max_eapi, pkg=new_pkg)
 