97558276231cdac3a1b323bfbc2e216d4be3afe4
Arthur Zamarin <arthurzam@gentoo.org>
2025-02-07 21:53:17 +0200

    testdata: fix patch files for mac os
    Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

---
+++ testdata/data/repos/standalone/PkgDirCheck/InvalidPN/fix.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mv PkgDirCheck/InvalidPN/InvalidPN.ebuild PkgDirCheck/InvalidPN/InvalidPN-0.ebuild
---
+++ testdata/data/repos/standalone/PkgDirCheck/MismatchedPN/fix.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mv PkgDirCheck/MismatchedPN/Mismatched-1.ebuild PkgDirCheck/MismatchedPN/MismatchedPN-1.ebuild
