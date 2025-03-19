pkgcore/pkgcheck

commit b86872a25614ca16177ca161638d8a0837cbda37

PythonCompatUpdate: ignore *t targets (freethreading)
Resolves: https://github.com/pkgcore/pkgcheck/issues/711
Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

src/pkgcheck/checks/python.py
@@ -577,7 +577,7 @@ class PythonCompatCheck(Check):
     ``python-any-r1``.
     """
 
-    known_results = frozenset([PythonCompatUpdate])
+    known_results = frozenset({PythonCompatUpdate})
 
     whitelist_backports = frozenset(
         {
@@ -600,15 +600,19 @@ class PythonCompatCheck(Check):
         # determine available PYTHON_TARGET use flags
         targets = []
         for target, _desc in repo.use_expand_desc.get(IUSE_PREFIX[:-1], ()):
-            if target[len(IUSE_PREFIX) :].startswith("python"):
-                targets.append(target[len(IUSE_PREFIX) :])
+            target = target.removeprefix(IUSE_PREFIX)
+            if target.startswith("python"):
+                targets.append(target)
+        targets = (x for x in targets if not x.endswith("t"))
         multi_targets = tuple(sorted(targets, key=self.sorter))
 
         # determine available PYTHON_SINGLE_TARGET use flags
         targets = []
         for target, _desc in repo.use_expand_desc.get(IUSE_PREFIX_S[:-1], ()):
-            if target[len(IUSE_PREFIX_S) :].startswith("python"):
-                targets.append(target[len(IUSE_PREFIX_S) :])
+            target = target.removeprefix(IUSE_PREFIX_S)
+            if target.startswith("python"):
+                targets.append(target)
+        targets = (x for x in targets if not x.endswith("t"))
         single_targets = tuple(sorted(targets, key=self.sorter))
 
         self.params = {

testdata/repos/python/profiles/desc/python_targets.desc
@@ -4,4 +4,5 @@ python3_7 - Build with Python 3.7
 python3_8 - Build with Python 3.8
 python3_9 - Build with Python 3.9
 python3_10 - Build with Python 3.10
+python3_13t - Build with Python 3.13 freethreading
 pypy3 - Build for PyPy3 only

commit beee75e768792dde4c558419375a72210203a608

python: Fix misfired PythonCompatUpdate when newest PYTHON_COMPAT is *t
Signed-off-by: Michał Górny <mgorny@gentoo.org>
Closes: https://github.com/pkgcore/pkgcheck/pull/732
Fixes: b86872a25614ca16177ca161638d8a0837cbda37
Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

src/pkgcheck/checks/python.py
@@ -658,7 +658,7 @@ class PythonCompatCheck(Check):
             all_targets = (
                 f"python{x.slot.replace('.', '_')}"
                 for x in deps
-                if x.key == "dev-lang/python" and x.slot is not None
+                if x.key == "dev-lang/python" and x.slot is not None and not x.slot.endswith("t")
             )
             latest_target = max(all_targets, key=self.sorter)
         except ValueError:

testdata/repos/python/PythonCompatCheck/PythonCompatUpdate/PythonCompatUpdate-4.ebuild
@@ -1,0 +1,11 @@
+EAPI=7
+PYTHON_COMPAT=( python3_{7..10} python3_13t pypy3 pypy3_11 )
+
+inherit python-any-r1
+
+DESCRIPTION="Ebuild with potential PYTHON_COMPAT updates"
+HOMEPAGE="https://github.com/pkgcore/pkgcheck"
+LICENSE="BSD"
+SLOT="0"
+
+DEPEND="${PYTHON_DEPS}"