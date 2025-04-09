pkgcore/pkgcheck

commit be92740c59c7dc0309c4e05f2f73ebdf339e2376

BadPackageUpdate: fix when not run inside profiles/ dir
When we weren't inside profiles/ dir, we were running the VisibilityCheck
check. This check was collecting repo.config.updates. This one is cached,
resulting in the value not being recomputed when time comes for
PackageUpdatesCheck to run. This commit fixes this by making sure we
invalidate the cache.

Sadly the object we're using to store config is Immutable, resulting in
a very ugly hack to invalidate the cache, by using the __dict__ attribute
of the object. This is not a good practice, but it works.

Reported-by: Eli Schwartz <eschwartz@gentoo.org>
Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

src/pkgcheck/checks/repo_metadata.py
@@ -105,6 +105,7 @@ class PackageUpdatesCheck(RepoCheck):
             base.LogMap("pkgcore.log.logger.error", BadPackageUpdate),
         )
 
+        del self.repo.config.__dict__["_updates"]  # clear cache for immutable dict
         # convert log warnings/errors into reports
         with base.LogReports(*logmap) as log_reports:
             repo_updates = self.repo.config.updates

testdata/data/repos/visibility/PackageUpdatesCheck/BadPackageUpdate/expected.json
@@ -1,0 +1,1 @@
+{"__class__": "BadPackageUpdate", "msg": "file '1Q-2024': 'move <=stub/old-name-1.2.3.4 stub/stable' on line 3: atom <=stub/old-name-1.2.3.4 must be versionless"}

testdata/data/repos/visibility/PackageUpdatesCheck/OldPackageUpdate/expected.json
@@ -1,0 +1,1 @@
+{"__class__": "OldPackageUpdate", "pkg": "stub/random-pkgname", "updates": ["move", "VisibilityCheck/OldPackageName", "stub/random-pkgname"]}

testdata/repos/visibility/profiles/updates/1Q-2024
@@ -1,2 +1,3 @@
 move stub/old-name stub/stable
 move VisibilityCheck/OldPackageName stub/random-pkgname
+move <=stub/old-name-1.2.3.4 stub/stable

commit c8c1d3a1339ab5e575d330dbc248ffd59c8907e2

BadPackageUpdate: fix when ran as only check
Fixes: be92740c59c7dc0309c4e05f2f73ebdf339e2376
Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

src/pkgcheck/checks/repo_metadata.py
@@ -105,7 +105,8 @@ class PackageUpdatesCheck(RepoCheck):
             base.LogMap("pkgcore.log.logger.error", BadPackageUpdate),
         )
 
-        del self.repo.config.__dict__["_updates"]  # clear cache for immutable dict
+        if hasattr(self.repo.config, "_updates"):
+            del self.repo.config.__dict__["_updates"]  # clear cache for immutable dict
         # convert log warnings/errors into reports
         with base.LogReports(*logmap) as log_reports:
             repo_updates = self.repo.config.updates