7e2120a792da448d58c238f03fe41c69adf8808c
Arthur Zamarin <arthurzam@gentoo.org>
2026-07-24 09:19:27 +0300

    start work on 0.10.42
    Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

--- src/pkgcheck/__init__.py
+++ src/pkgcheck/__init__.py
@@ -4,6 +4,6 @@ from .results import Result
 
 __all__ = ("PkgcheckException", "Result", "scan")
 __title__ = "pkgcheck"
-__version__ = "0.10.41"
-__version_info__ = (0, 10, 41)
+__version__ = "0.10.42.dev0"
+__version_info__ = (0, 10, 42, "dev0")
 __python_mininum_version__ = (3, 12, 0)
